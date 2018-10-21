9c556315f4a946d73de61a32a26b56b2f39f5216
MINETA HATSUNE <1029286276@example.com>
2018-10-21 14:09:31 +0900

    Initial commit

---
+++ Example/EasyImagePicker/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  EasyImagePicker
+//
+//  Created by hatsunem on 10/21/2018.
+//  Copyright (c) 2018 hatsunem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
